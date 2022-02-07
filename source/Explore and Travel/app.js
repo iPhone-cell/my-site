8707f05b17cf34b94171dc0691e66cc0fbda19d8
iPhone-cell <53273668+iPhone-cell@users.noreply.github.com>
2022-02-07 14:35:10 +0300

    Add files via upload

---
+++ source/Explore and Travel/app.js
@@ -0,0 +1,48 @@
+$(function() {
+
+    // Variables
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    // Fixed Header
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH); 
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    
+    // Nav Toggle
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+    // Testimonials: https://kenwheeler.github.io/slick/
+    let slider = $("#testimonialsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+});
