d534223e5646160dfe964b4533f131514a27df0a
iPhone-cell <53273668+iPhone-cell@users.noreply.github.com>
2022-02-07 14:36:52 +0300

    Add files via upload

---
+++ source/Lugar/js/app.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    // Mobile nav
+
+    const navToggle = $("#navToggle");
+    const nav = $("#nav");
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+});
